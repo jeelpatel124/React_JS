3aa0d8802826af0cc3185979d88bb6be944fad8a
jeelpatel124 <nananiyajeel77@gmail.com>
2024-12-07 23:45:26 +0530

    login

---
+++ day13/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Login from './component/login'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Login />
+      
+    </>
+  )
+}
+
+export default App
---
+++ day13/src/component/data.jsx
@@ -0,0 +1,18 @@
+function Show(p){
+
+    
+    {console.log(p.p_value)}
+    
+    return( <div>
+
+                {/* {p.p_value.map((e)=>{
+                    return (
+                        <div>
+                            <p> {e.pass} </p>
+                        </div>
+                    )
+                })} */}
+    </div> ) 
+}
+
+export default Show
